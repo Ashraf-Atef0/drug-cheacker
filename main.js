f8830d0e94a1fa26b4e4f13849c95f53c9cc6f1f
Ashraf Atef <ashraf.programer@gmail.com>
2023-03-19 07:22:10 +0200

    adding fix interaction pug

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let searchBtn = document.getElementById("search");
 let interactionBtn = document.getElementById("interaction-checker");
 let drugsContainer = document.querySelector(".drug-list");
 let searchCount = 0;
-let getInteraction = false;
+let getInteraction = true;
 let interactionList = [];
 let interactionComments = [];
 
@@ -24,7 +24,7 @@ searchBtn.addEventListener("click", () => {
   if (searchCount > 0) {
     drugsContainer.firstElementChild.remove();
   }
-  getDataBase(inputFeld.value, 1);
+  getDataBase(inputFeld.value.toLowerCase(), 1);
   searchCount++;
 });
 
@@ -92,7 +92,264 @@ function generateDrugCards(drugLists) {
       drugMoreBtn
     );
     allDrugCards.append(drugCard);
-    console.log(drugCard);
   });
   drugsContainer.appendChild(allDrugCards);
 }
+
+document.addEventListener("click", (e) => {
+  if (e.target.className == "drug-Alt-eg") {
+    let drugCode = e.target.parentElement.getAttribute("drug-code");
+    drugsContainer.firstElementChild.remove();
+    getDataBase(drugCode, 2);
+  } else if (e.target.className == "drug-Sim-eg") {
+    let drugCode = e.target.parentElement.getAttribute("drug-code");
+    drugsContainer.firstElementChild.remove();
+    getDataBase(drugCode, 3);
+  } else if (e.target.className == "drug-more-eg") {
+    let moreRequest = new XMLHttpRequest();
+    moreRequest.onreadystatechange = () => {
+      if (moreRequest.readyState == 4) {
+        console.log(moreRequest.responseText);
+        let htmlText = moreRequest.responseText,
+          sliceStart = htmlText.lastIndexOf("<td"),
+          sliceEnd = htmlText.lastIndexOf("</td>");
+        let preTreatedText = htmlText.slice(sliceStart, sliceEnd);
+        let treatedText = preTreatedText.slice(preTreatedText.indexOf(">") + 1);
+        e.target.classList.add("active-pop");
+        let moreInformation = document.createElement("p");
+        moreInformation.className = "pop-info";
+        moreInformation.innerHTML = treatedText;
+        e.target.parentElement.append(moreInformation);
+      }
+    };
+    moreRequest.open(
+      "get",
+      "http://www.drugeye.pharorg.com/drugeyeapp/android-search/drugeye-android-live-more.aspx?gname=" +
+        e.target.parentElement.firstElementChild.innerText,
+      true
+    );
+    moreRequest.send();
+  } else if (e.target.classList.contains("drug-card-eg") && getInteraction) {
+    console.log("clicked");
+    let apiName = e.target.children.item(1).innerText;
+    getApiNumber(apiName, 1);
+    // if (firstCheck) {
+    //   let rxCode = firstCheck[0];
+    //   interactionList.push(rxCode);
+    //   console.log(rxCode);
+    // } else if (apiName.indexOf("+") != -1) {
+    //   apiName.split("+").map((eachName) => {
+    //     let mulitCheck = getApiNumber(eachName);
+    //     if (mulitCheck) {
+    //       let rxCode = mulitCheck[0];
+    //       interactionList.push(rxCode);
+    //       console.log(rxCode);
+    //     } else {
+    //       console.log("sorry there is no data about" + eachName);
+    //     }
+    //   });
+    // }
+    ///////////////////////////////////////////////////////////////////
+    // if (e.target.classList.contains("selected")) {
+    //   e.target.classList.remove("selected");
+    // } else {
+    //   let apiName = e.target.children.item(1).innerText;
+    //   console.log(apiName);
+    //   let codeRequest = new XMLHttpRequest();
+    //   codeRequest.onreadystatechange = () => {
+    //     if (codeRequest.readyState == 4) {
+    //       let responseObject = JSON.parse(codeRequest.response);
+
+    //       if (!responseObject.idGroup.rxnormId) {
+    //         let rxCode = responseObject.idGroup.rxnormId[0];
+    //       } else {
+    //         let rxCode = null;
+    //       }
+    //       if (interactionList.indexOf(rxCode) == -1 && rxCode) {
+    //         interactionList.push(rxCode);
+    //         console.log(interactionList);
+    //       } else {
+    //         if (apiName.indexOf("+") == -1) {
+    //           let apiNewName = apiName.split(" ")[0];
+    //           let newCodeRequest = new XMLHttpRequest();
+    //           newCodeRequest.onreadystatechange = () => {
+    //             if (codeRequest.readyState == 4) {
+    //               let responseObject = JSON.parse(codeRequest.response);
+    //               if (!responseObject.idGroup.rxnormId) {
+    //                 let rxCode = responseObject.idGroup.rxnormId[0];
+    //               } else {
+    //                 let rxCode = null;
+    //               }
+    //               if (interactionList.indexOf(rxCode) == -1 && rxCode) {
+    //                 interactionList.push(rxCode);
+    //                 console.log(interactionList);
+    //               } else {
+    //                 // need to edit it;
+    //                 console.log("Sorry there is no data about this drug");
+    //               }
+    //             }
+    //           };
+    //           codeRequest.open(
+    //             "get",
+    //             `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${apiNewName}&search=1`,
+    //             true
+    //           );
+    //           codeRequest.send();
+    //         } else {
+    //           //
+    //           let apiNewNameList = apiName.split("+");
+    //           apiNewNameList.map((apiNewName) => {
+    //             let newCodeRequest = new XMLHttpRequest();
+    //             newCodeRequest.onreadystatechange = () => {
+    //               if (codeRequest.readyState == 4) {
+    //                 let responseObject = JSON.parse(codeRequest.response);
+    //                 if (!responseObject.idGroup.rxnormId) {
+    //                   let rxCode = responseObject.idGroup.rxnormId[0];
+    //                 } else {
+    //                   let rxCode = null;
+    //                 }
+    //                 if (interactionList.indexOf(rxCode) == -1 && rxCode) {
+    //                   interactionList.push(rxCode);
+    //                   console.log(interactionList);
+    //                 } else {
+    //                   // need to edit it;
+    //                   console.log("Sorry there is no data about this drug");
+    //                 }
+    //               }
+    //             };
+    //             codeRequest.open(
+    //               "get",
+    //               `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${apiNewName}&search=1`,
+    //               true
+    //             );
+    //             codeRequest.send();
+    //           });
+    //         }
+    //       }
+    //       console.log(rxCode);
+    //     }
+    //   };
+    //   codeRequest.open(
+    //     "get",
+    //     `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${apiName}&search=1`,
+    //     true
+    //   );
+    //   codeRequest.send();
+    //   e.target.classList.add("selected");
+    // }
+  }
+});
+
+async function getApiNumber(drugName, state) {
+  if (state == 1) {
+    console.log("hello1");
+    let codeRequest = new XMLHttpRequest();
+    codeRequest.onreadystatechange = async () => {
+      if (codeRequest.readyState == 4) {
+        let responseObject = await JSON.parse(codeRequest.response);
+        console.log(responseObject, "from await");
+        let rxList = responseObject.idGroup.rxnormId;
+        if (rxList) {
+          interactionList.push(rxList[0]);
+          console.log(rxList);
+        } else {
+          if (drugName.indexOf("+") == -1) {
+            getApiNumber(drugName.split(" ")[0], 2);
+          } else {
+            getApiNumber(drugName.split("+"), 3);
+          }
+        }
+        // return responseObject.idGroup.rxnormId;
+      }
+    };
+    codeRequest.open(
+      "get",
+      `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drugName}&search=1`,
+      true
+    );
+    codeRequest.send();
+  } else if (state == 2) {
+    console.log("hello2");
+    let codeRequest = new XMLHttpRequest();
+    codeRequest.onreadystatechange = async () => {
+      if (codeRequest.readyState == 4) {
+        let responseObject = await JSON.parse(codeRequest.response);
+        console.log(responseObject, "from await");
+        let rxList = responseObject.idGroup.rxnormId;
+        if (rxList) {
+          interactionList.push(rxList[0]);
+          console.log(rxList);
+        } else {
+          console.log("sorry no data state 2");
+        }
+        // return responseObject.idGroup.rxnormId;
+      }
+    };
+    codeRequest.open(
+      "get",
+      `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drugName}&search=1`,
+      true
+    );
+    codeRequest.send();
+  } else if (state == 3) {
+    console.log("hello3");
+    drugName.map((drugNamePart) => {
+      let codeRequest = new XMLHttpRequest();
+      codeRequest.onreadystatechange = async () => {
+        if (codeRequest.readyState == 4) {
+          let responseObject = await JSON.parse(codeRequest.response);
+          console.log(responseObject, "from await");
+          let rxList = responseObject.idGroup.rxnormId;
+          if (rxList) {
+            interactionList.push(rxList[0]);
+            console.log(rxList);
+          } else {
+            console.log("sorry no data state 3");
+          }
+          // return responseObject.idGroup.rxnormId;
+        }
+      };
+      codeRequest.open(
+        "get",
+        `https://rxnav.nlm.nih.gov/REST/rxcui.json?name=${drugNamePart}&search=1`,
+        true
+      );
+      codeRequest.send();
+    });
+  }
+}
+
+interactionBtn.onclick = () => {
+  console.log(interactionList);
+  if (interactionList.length > 1) {
+    let interactionRequest = new XMLHttpRequest();
+    interactionRequest.onreadystatechange = () => {
+      if (interactionRequest.readyState == 4) {
+        let interactionObject = JSON.parse(interactionRequest.response);
+        let interactionGroups = interactionObject.fullInteractionTypeGroup;
+        if (interactionGroups) {
+          interactionGroups.map((gruop) => {
+            gruop.fullInteractionType.map((inters) => {
+              inters.interactionPair.map((interText) => {
+                if (interactionComments.indexOf(interText.description) == -1) {
+                  interactionComments.push(interText.description);
+                }
+                //   console.log(interText.description);
+              });
+            });
+          });
+        }
+        console.log(interactionComments);
+        // console.log(interactionGroups);
+      }
+    };
+    interactionRequest.open(
+      "get",
+      `https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=${interactionList.join(
+        "+"
+      )}`,
+      true
+    );
+    interactionRequest.send();
+  }
+};
